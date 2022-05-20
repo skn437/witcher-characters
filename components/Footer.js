5569092c81a1ca6ad88f9025169d0995b86d9f4c
SKN <skn437physx@gmail.com>
2022-05-20 17:13:33 +0600

    Styled Footer.js a bit

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,10 @@
+import styles from "../styles/Footer.module.css";
+
 const Footer = () => {
 
     return (
 
-        <footer id="copyright">
+        <footer id={styles.copyright}>
 
             <p>Copyright &copy; : SHUKHAN 2022</p>
 
